444b8d9855a29bd41da952fac38608cf94e3c22e
tchelex <tchelex@mail.ru>
2020-04-20 09:26:44 +0300

    step 2

--- test_django/urls.py
+++ test_django/urls.py
@@ -6,4 +6,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('landing.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
